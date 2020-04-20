2f91050193648e8a3f5cc3ee56e1ef29f15f41a4
JonathanESchmidt <33545029+JonathanESchmidt@users.noreply.github.com>
2020-04-20 10:46:15 +0200

    Setup

--- pipline/examples/pre_proecc.py
+++ pipline/examples/pre_proecc.py
@@ -2,9 +2,8 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-#img = cv2.imread("./images/")
+img = cv2.imread("C:\\Users\\Jonathan\\Source\\Repos\\WNA\\pipline\\examples\\TestBillede.png")
 
-#cv2.imshow("img", img)
-
+cv2.imshow("img", img)
 key = cv2.waitKey(0)
 
