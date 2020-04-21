310bb0db4727f4a9270cffc6f6ac767605e9b1b7
JonathanESchmidt <33545029+JonathanESchmidt@users.noreply.github.com>
2020-04-21 12:48:33 +0200

    Added Trackbar for Canny detection

--- pipline/examples/EdgeTest.py
+++ pipline/examples/EdgeTest.py
@@ -53,3 +53,50 @@ while True:
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+
+
+
+
+'''
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+image = cv2.imread("C:\\Users\\Jonathan\\Source\\Repos\\WNA\\pipline\\examples\\TestBillede2.png")
+img1 = cv2.GaussianBlur(image, (7, 7), 0)
+img2 = cv2.bilateralFilter(image, 7, 50, 50)
+canny1 = cv2.Canny(img1, 70, 100)
+canny2 = cv2.Canny(img2, 50, 80)
+
+bit_not1 = cv2.bitwise_not(canny1)
+bit_not2 = cv2.bitwise_not(canny2)
+bit_and1 = cv2.bitwise_and(img1, img1, mask=bit_not1)
+bit_and2 = cv2.bitwise_and(img2, img2, mask=bit_not2)
+
+
+lines = cv2.HoughLinesP(canny1, 1, np.pi/180, 30, maxLineGap=5)
+
+for line in lines:
+    x1, y1, x2, y2 = line[0]
+    cv2.line(img1, (x1, y1), (x2, y2), (0, 0, 255), 3)
+
+cv2.imshow("Edges", canny1)
+cv2.imshow("Image", img1)
+cv2.imshow("Normal", image)
+
+#diff = bit_and1 - bit_and2
+#diff = canny1 - canny2
+
+#img_gray = cv2.cvtColor(bit_and, cv2.COLOR_BGR2GRAY)
+
+#cv2.imshow("Image", img)
+#cv2.imshow("Canny1", canny1)
+#cv2.imshow("And1", bit_and1)
+#cv2.imshow("Canny2", canny2)
+#cv2.imshow("And2", bit_and2)
+#cv2.imshow("Diff", diff)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+'''
--- pipline/examples/edgePrepross.py
+++ pipline/examples/edgePrepross.py
@@ -2,28 +2,42 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-image = cv2.imread("C:\\Users\\Jonathan\\Source\\Repos\\WNA\\pipline\\examples\\TestBillede.png")
-img1 = cv2.GaussianBlur(image, (9, 9), 0)
-img2 = cv2.bilateralFilter(image, 7, 50, 50)
-canny1 = cv2.Canny(img1, 50, 80)
-canny2 = cv2.Canny(img2, 50, 80)
+def nothing(x):
+    pass
 
-bit_not1 = cv2.bitwise_not(canny1)
-bit_not2 = cv2.bitwise_not(canny2)
-bit_and1 = cv2.bitwise_and(img1, img1, mask=bit_not1)
-bit_and2 = cv2.bitwise_and(img2, img2, mask=bit_not2)
+image = cv2.imread("C:\\Users\\Jonathan\\Source\\Repos\\WNA\\pipline\\examples\\TestBillede2.png")
+cv2.namedWindow("Trackbars")
+cv2.createTrackbar("MaxVal", "Trackbars", 80, 255, nothing)
+cv2.createTrackbar("MinVal", "Trackbars", 25, 255, nothing)
 
-#diff = bit_and1 - bit_and2
-#diff = canny1 - canny2
 
-#img_gray = cv2.cvtColor(bit_and, cv2.COLOR_BGR2GRAY)
+while True:
+    img1 = cv2.GaussianBlur(image, (7, 7), 0)
+    img2 = cv2.bilateralFilter(image, 7, 50, 50)
 
-#cv2.imshow("Image", img)
-cv2.imshow("Canny1", canny1)
-cv2.imshow("And1", bit_and1)
-cv2.imshow("Canny2", canny2)
-cv2.imshow("And2", bit_and2)
-cv2.imshow("Diff", diff)
+    maxVal = cv2.getTrackbarPos("MaxVal", "Trackbars")
+    minVal = cv2.getTrackbarPos("MinVal", "Trackbars")
 
-cv2.waitKey(0)
+    canny1 = cv2.Canny(img1, minVal, maxVal)
+    canny2 = cv2.Canny(img2, minVal, maxVal)
+
+    bit_not1 = cv2.bitwise_not(canny1)
+    bit_not2 = cv2.bitwise_not(canny2)
+    bit_and1 = cv2.bitwise_and(img1, img1, mask=bit_not1)
+    bit_and2 = cv2.bitwise_and(img2, img2, mask=bit_not2)
+
+    diff = bit_and1 - bit_and2
+    #diff = canny1 - canny2
+
+    #cv2.imshow("Image", img)
+    cv2.imshow("Canny1", canny1)
+    cv2.imshow("And1", bit_and1)
+    cv2.imshow("Canny2", canny2)
+    cv2.imshow("And2", bit_and2)
+    #cv2.imshow("Diff", diff)
+    
+    key = cv2.waitKey(100)
+    if key == 27:
+        break
+
 cv2.destroyAllWindows()
