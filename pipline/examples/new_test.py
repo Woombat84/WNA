c864cc86decfa92b4f4a31c9d58d9d9d704bb075
Woombat84 <33546737+Woombat84@users.noreply.github.com>
2020-04-30 13:27:25 +0200

    a small update in new_test, small sections of code added

--- pipline/examples/new_test.py
+++ pipline/examples/new_test.py
@@ -18,9 +18,28 @@ import matplotlib.pyplot as plt
 #segmentation
 
 #bluring
+#edge detection
 #thresholding on color hsv
-def segmention(img):
+
+# this function segment the input image
+# depened on what representation the image
+# is in the output is a binary image. 
+def segmention(img,color):
+    #0 color
+    #1 HSV
+    #2 gray
+    if color == 0:
+        blur_img = cv2.GaussianBlur(img, (9, 9), 0)
+        canny = cv2.Canny(blur_img, 50, 80)
+
     
+    if color == 1:
+        blur_img = cv2.GaussianBlur(img, (9, 9), 0)
+        canny = cv2.Canny(blur_img, 50, 80)
+        
+    if color == 2:
+        blur_img = cv2.GaussianBlur(img, (9, 9), 0)
+        retval, seg_img = cv2.threshold(img_gray, thresh, 255, cv2.THRESH_BINARY)
     
     return seg_img
         
@@ -29,17 +48,25 @@ def segmention(img):
 #feature extracting
 
 
-#edge detection 
-#contures
+# find colors
+# find contures
 
 # this function recives a binary image and a colored 
-# image, and returns a list of features colors, perimeter length and area.
+# image, and returns a list of features colorsRGB, colorsHSV,
+# perimeter length and area further investigation skeleton features.
 
-def feature_ex():
+def feature_ex(img,hsv,gray):
+    # find colors
 
-
+    # find countures 
+    col_contours, col_hierarchy	= cv2.findContours(img)
+    hsv_contours, hsv_hierarchy	= cv2.findContours(hsv)
+    gray_contours, gray_hierarchy = cv2.findContours(gray)
 
     return lst_features
+
+
+
 
 #now that the features are extracted, the features can be used for 
 #either training or evealuting the weed number based on the tranined data
@@ -54,14 +81,20 @@ def traning_data():
 # output a weed number on trained data
 def weed_number():
     pass
+
+
+
+
 #now the resaspie is determined, it is time to think about loading images 
 #into the process 
 
 #getting a path for a image 
-#returing a colored image
+#returing a colored, HSV representaion and grayscaled image
 def load_image(path):
-    img = cv2.imread(path,cv2.IMREAD_COLOR)
-    return img
+    col_img = cv2.imread(path,cv2.IMREAD_COLOR)
+    gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    hsv_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    return col_img, gray_img, hsv_img
 
 
 
@@ -86,10 +119,24 @@ def listDir():
 
         os.chdir(retval)
 
-def main():
+def resipe():
+    col_img, hsv_img, gray_img = load_image(" ") 
     
-    segmention()
+    bi_col_img = segmention(col_img,0)
+    bi_hsv_img = segmention(hsv_img,1)
+    bi_gray_img = segmention(gray_img,2)
+
+
+def main():
+    path = "C:\\Users\\WoomBat\\Aalborg Universitet\\Jonathan Eichild Schmidt - P6 - billeder\\cropped_weednumber_sorted\\10_04282020_01\\011.jpeg"
+    col_img, hsv_img, gray_img = load_image(path) 
+    
+    bi_col_img = segmention(col_img,0)
+
+    
+    
     exit(1)
+
 
 if __name__ == "__main__":
     # execute only if run as a script
