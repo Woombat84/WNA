447052211d6d1bcb8223c8d7bb28ac4f805261ea
Woombat84 <33546737+Woombat84@users.noreply.github.com>
2020-04-30 11:32:55 +0200

    added new start layout no fuctionalty as of yet

---
+++ pipline/examples/new_test.py
@@ -0,0 +1,33 @@
+"""
+This is just a rouhg layout
+"""
+import cv2
+import time
+
+import numpy as np
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+import pandas as pd
+
+
+import matplotlib.pyplot as plt
+
+
+#image aqustion and pre-prosseccing is now done it is time for 
+#segmentation
+
+
+
+
+#now that segmentaion is done it is time for 
+#feature extracting
+
+
+
+
+#now that the features are extracted, the features can be used for 
+#either training or evealuting the weed number based on the tranined data
+#classification
+
+
+  
